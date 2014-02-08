0fa158e5fd2e558be78c18ce7ab0c427e14e199c
Rudy Rigot <rudy.rigot@gmail.com>
2014-02-07 16:20:32 -0800

    Fixing link bugs and applying best practice to links

--- js/prismic-configuration.js
+++ js/prismic-configuration.js
@@ -16,34 +16,38 @@ define(function() {
     linkResolver: function(ctx, doc, isBroken) {
 
       if(doc.id == ctx.api.bookmarks['about']) {
-        return '#about' + ctx.maybeRefParam;
+        return '/#about' + ctx.maybeRefParam;
       }
 
       if(doc.id == ctx.api.bookmarks['jobs']) {
-        return '#jobs' + ctx.maybeRefParam;
+        return '/#jobs' + ctx.maybeRefParam;
       }
 
       if(doc.id == ctx.api.bookmarks['stores']) {
-        return '#stores' + ctx.maybeRefParam;
+        return '/#stores' + ctx.maybeRefParam;
       }
 
       if(doc.type == 'store' && !isBroken) {
-        return '#stores' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+        return '/#stores' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
       }
 
       if(doc.type == 'product' && !isBroken) {
-        return '#products' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+        return '/#products' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+      }
+
+      if(doc.type == 'selection' && !isBroken) {
+        return '/#selections' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
       }
 
       if(doc.type == 'job-offer' && !isBroken) {
-        return '#jobs' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+        return '/#jobs' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
       }
 
       if(doc.type == 'blog-post' && !isBroken) {
         return '/blog.html#posts' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
       }
 
-      return '#not-found';
+      return '/#not-found';
     }
 
   };
